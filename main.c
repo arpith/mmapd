18de04159b06fb9a1cf6b18abbba2179dac74057
Arpith Siromoney <arpith@feedreader.co>
2016-06-03 15:18:06 +0530

    accept input in an infinite loop

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
         char *line = NULL;
         size_t size;
-        if (getline(&line, &size, stdin) == -1) {
-                printf("No line");
-        } else {
-                printf("%s\n", line);
+        while(true) {
+                if (getline(&line, &size, stdin) == -1) {
+                        printf("No line");
+                } else {
+                        printf("%s\n", line);
+                }
         }
         return 0;
 }
