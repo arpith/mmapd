b8940af163b0159d2c8476bf2a58b44c4cbae8aa
Arpith Siromoney <arpith@feedreader.co>
2016-07-05 14:54:13 -0400

    memory map db file, creating it if necessary

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
@@ -18,13 +19,26 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if _, err := os.Stat("db"); os.IsNotExist(err) {
+		os.Create("db")
+	}
+
+	db, err := mmap.Open("db")
+	if err != nil {
+		fmt.Print("Error memory mapping db file: ", err)
+	}
+
+	len := db.Len
+
+	fmt.Printf("Length of memory mapped db file is: %d", len)
+
 	router := httprouter.New()
-	router.HandlerFunc("POST", "/set/:key", setHandler())
-	router.HandlerFunc("GET", "/get/:key", getHandler())
+	router.HandlerFunc("POST", "/set/:key", setHandler)
+	router.HandlerFunc("GET", "/get/:key", getHandler)
 
 	port := strings.TrimSpace(os.Getenv("PORT"))
 	if port == "" {
-		port = 3001
+		port = "3001"
 	}
 	http.ListenAndServe(":"+port, router)
 }
