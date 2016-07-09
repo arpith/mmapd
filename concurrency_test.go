12b02ba434078923c71d1cb80b059707ebaf3182
Arpith Siromoney <arpith@feedreader.co>
2016-07-09 14:38:54 -0400

    test now checks the get request too

--- concurrency_test.go
+++ concurrency_test.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"testing"
 )
 
 func TestConcurrency(t *testing.T) {
-	for i := 0; i < 500; i++ {
+	maxRequests := os.Getenv("MAX_REQUESTS")
+	max, err := strconv.Atoi(maxRequests)
+	if err != nil {
+		max = 250
+	}
+	for i := 0; i < max; i++ {
 		s := strconv.Itoa(i)
 		fmt.Println("Setting", s)
-		_, err := http.PostForm("http://localhost:3001/set/"+s, url.Values{"value": {s}})
+		resp, err := http.PostForm("http://localhost:3001/set/"+s, url.Values{"value": {s}})
 		if err != nil {
-			fmt.Println("Error setting ", s, ": ", err)
+			t.Error("Expected ", s, " got ", err)
 		}
+		defer resp.Body.Close()
+	}
+	for i := 0; i < max; i++ {
+		s := strconv.Itoa(i)
+		resp, err := http.Get("http://localhost:3001/get/" + s)
+		if err != nil {
+			t.Error("Expected ", s, " got ", err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil || string(body) != s {
+			t.Error("Expected ", s, " got ", err)
+		}
+		defer resp.Body.Close()
 	}
 }
