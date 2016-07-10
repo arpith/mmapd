766ac4b45d2f9992e4f22033957a2544aaba8571
Arpith Siromoney <arpith@feedreader.co>
2016-07-09 21:37:25 -0400

    add tests for setting / getting values

--- concurrency_test.go
+++ concurrency_test.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"crypto/rand"
+	"encoding/base64"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -9,6 +10,59 @@ import (
 	"strconv"
 	"testing"
 )
+
+func TestSet(t *testing.T) {
+	c := 9
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		t.Error("Couldn't generate random string: ", err)
+	}
+	s := base64.URLEncoding.EncodeToString(b)
+	resp, err := http.PostForm("http://localhost:3001/set/"+s, url.Values{"value": {s}})
+	if err != nil {
+		t.Error("Expected ", s, " got ", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error("Expected ", s, " got ", err)
+	} else if string(body) != s {
+		t.Error("Expected ", s, " got ", string(body))
+	}
+}
+
+func TestGet(t *testing.T) {
+	c := 9
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		t.Error("Couldn't generate random string: ", err)
+	}
+	s := base64.URLEncoding.EncodeToString(b)
+	resp, err := http.PostForm("http://localhost:3001/set/"+s, url.Values{"value": {s}})
+	if err != nil {
+		t.Error("Couldn't set ", s, ": ", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error("Expected ", s, " got ", err)
+	} else if string(body) != s {
+		t.Error("Expected ", s, " got ", err)
+	}
+	resp, err = http.Get("http://localhost:3001/get/" + s)
+	if err != nil {
+		t.Error("Couldn't get ", s, ": ", err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error("Expected ", s, " got ", err)
+	} else if string(body) != s {
+		t.Error("Expected ", s, " got ", err)
+	}
+}
 
 func TestConcurrency(t *testing.T) {
 	maxRequests := os.Getenv("MAX_REQUESTS")
@@ -18,7 +72,6 @@ func TestConcurrency(t *testing.T) {
 	}
 	for i := 0; i < max; i++ {
 		s := strconv.Itoa(i)
-		fmt.Println("Setting", s)
 		resp, err := http.PostForm("http://localhost:3001/set/"+s, url.Values{"value": {s}})
 		if err != nil {
 			t.Error("Expected ", s, " got ", err)
